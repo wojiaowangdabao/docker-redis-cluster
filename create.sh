9b1d7ecb1d4e618d6f178302b139d730be10cd94
wojiaowangdabao <1481173027@qq.com>
2026-03-27 15:09:36 +0800

    test

---
+++ create.sh
@@ -0,0 +1,1 @@
+docker exec -it redis-1 sh -c "redis-cli -a my_secure_password_123456 --cluster create redis-1:6379 redis-2:6379 redis-3:6379 redis-4:6379 redis-5:6379 redis-6:6379 --cluster-replicas 1"
